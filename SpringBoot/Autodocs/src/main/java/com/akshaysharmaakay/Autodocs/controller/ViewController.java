c6be02e56abf63d0fd8a921d1c8f74bd1ab4a4f8
AkshaySharma-Akay <akshaysharmaak2@gmail.com>
2020-11-29 22:42:06 +0530

    Initial setup done and view controller added

---
+++ SpringBoot/Autodocs/src/main/java/com/akshaysharmaakay/Autodocs/controller/ViewController.java
@@ -0,0 +1,13 @@
+package com.akshaysharmaakay.Autodocs.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ViewController {
+
+	@RequestMapping(value= {"","/","/home"})
+	public String index() {
+		return "index";
+	}
+}
---
+++ SpringBoot/Autodocs/src/test/java/com/akshaysharmaakay/Autodocs/AutodocsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akshaysharmaakay.Autodocs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutodocsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

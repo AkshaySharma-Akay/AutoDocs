a9e0182855df440068d152228a2113efb39454ed
AkshaySharma-Akay <akshaysharmaak2@gmail.com>
2021-08-19 10:26:06 +0530

    Added routes, services and ejs template

--- Core/routes/genPdfRoutes.js
+++ Core/routes/genPdfRoutes.js
@@ -7,8 +7,14 @@ router.get('', (req,res)=>{
     return res.status(403).json({"status":"invalid request"});
 });
 router.post('', upload.single('file'), (req, res)=>{
-    generatePdfService.generateReport(req.file.path, req.body);
-    return res.status(200).json({"status":"processit"});
+    generatePdfService.generateReport(req.file.path, req.body)
+        .then((pdf)=>{
+            console.log("pdf - ", pdf);
+            return res.status(200).send(pdf);
+        })
+        .catch((error)=>{
+            res.send(500).send({"status":"Somthing went wrong" +error});
+        })
 });
 
 module.exports = router;
--- Core/services/generatePdfService.js
+++ Core/services/generatePdfService.js
@@ -1,27 +1,67 @@
 const xlsx2json = require('xlsx2json');
+const ejs = require('ejs');
+const path = require('path');
+let html2pdf = require('html2pdf');
 
-let getExcelData = (filePath) => {
-    return new Promise((resolve, reject) => {
-        try{
-            xlsx2json(filePath).then(jsonArray => {
+function getJson(filePath) {
+    return new Promise(function (resolve, reject) {
+        options = {
+            mapping: {
+                'sno': 'A',
+                'name': 'B',
+                'enrollment': 'C',
+                'marks': 'D',
+            }
+        };
+        xlsx2json(filePath, options)
+            .then((jsonArray) => {
                 resolve(jsonArray);
+            })
+            .catch((error) => {
+                reject(Error(error));
             });
-        } catch (exception){
-            console.log("exception occured " + e);
-            reject("can't read file");
-        }
-    })
-};
-
-let generateReport = (filePath, reqBody) => {
-    getExcelData(filePath).then((data,error)=>{
-        if(error) console.log(error);
-        else{
-            rows = data[0];
-            let headers = Object.values(rows[0]);
-            console.log(headers);
-        }
     });
+}
+
+function createHTMLReport(data, recordsLimitPerPage) {
+    return new Promise((resolve, reject) => {
+        ejs.renderFile(path.join(__dirname, "../views/reportTemplate.ejs"),
+            { "data":data, "recordsLimitPerPage":recordsLimitPerPage }, function (error, htmlString) {
+                if (error) {
+                    reject(error);
+                } else {
+                    resolve(htmlString);
+                }
+            });
+    })
+}
+
+async function convertHTMLToPdf(html) {
+    const html_to_pdf = require('html-pdf-node');
+    let options = { format: 'A4' , path:""};
+
+    let file = { content: html };
+
+    return await html_to_pdf.generatePdf(file, options);
+}
+
+async function generateReport(filePath, reqBody) {
+    let recordsLimitPerPage = 7;
+    try {
+        let jsonData = await getJson(filePath);
+        if (Array.isArray(jsonData) === true) {
+            sheet1Data = jsonData[0];
+            let htmlString = await createHTMLReport(sheet1Data, recordsLimitPerPage);
+            let pdf = await convertHTMLToPdf(htmlString);
+            return pdf;
+        }
+        else {
+            throw new Error(jsonData);
+        }
+    } catch (error) {
+        console.log(error);
+        return Error(error);
+    }
 }
 
 module.exports = { generateReport };
